b1391ac3a41807921b3f6d77244982f5fc396307
sirMikeee <mike@stcecilia.edu.ph>
2024-08-28 16:49:16 +0800

    I midified the maiun class

--- src/javaapp2e/JavaApp2E.java
+++ src/javaapp2e/JavaApp2E.java
@@ -1,21 +1,40 @@
 
 package javaapp2e;
 
+import java.util.Scanner;
+
 public class JavaApp2E {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
-//        Grades gr = new Grades();
-//        gr.addGrades(1011, "Mike Gwapo", 1.0, 2.2, 2.3, 1.3);
-//        gr.viewGrades();
-
-            Products pr = new Products();
-            pr.addProducts(1011, "Sabon", 30.00, 60, 0);
-            pr.viewProducts();
+        Products[] pr = new Products[100];
+        
+        System.out.print("Enter no. of Products: ");
+        int np = sc.nextInt();
+        
+        for(int i=0; i < np; i++){
+            System.out.println("Enter details of product "+(i+1));
+            System.out.print("ID: ");
+            int id = sc.nextInt();
+            System.out.print("Name:  ");
+            String name = sc.next();
+            System.out.print("Price: ");
+            double price = sc.nextDouble();
+            System.out.print("Stocks: ");
+            int stocks = sc.nextInt();
+            System.out.print("Sold: ");
+            int sold = sc.nextInt();
             
-            Products pr1 = new Products();
-            pr1.addProducts(1011, "Sabon", 30.00, 0, 0);
-            pr1.viewProducts();
-
+            pr[i] = new Products();
+            pr[i].addProducts(id, name, price, stocks, sold);
+        }
+        double tpg = 0;
+        for(int i=0; i < np; i++){
+            tpg = tpg + (pr[i].price * pr[i].sold);
+            pr[i].viewProducts();
+        }
+        
+        System.out.println("Total Profits Gained: "+ tpg);
     }
 }
